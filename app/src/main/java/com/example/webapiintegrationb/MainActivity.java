3f367c0151b50dd2d164d9441b1bfd7b2034928a
Syed Muhammad Saad Salman <im.saadsalmanahashmi@gmail.com>
2022-11-07 14:03:36 +0500

    Get Request Included

--- app/src/main/java/com/example/webapiintegrationb/MainActivity.java
+++ app/src/main/java/com/example/webapiintegrationb/MainActivity.java
@@ -10,6 +10,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,13 +43,40 @@ public class MainActivity extends AppCompatActivity {
         rv.setAdapter(adapter);
         rv.setLayoutManager(lm);
 
-        ls.add(new MyContact("sdvfd","dvd","dsvfv"));
-        ls.add(new MyContact("sdvfd","dvd","dsvfv"));
-        ls.add(new MyContact("sdvfd","dvd","dsvfv"));
-        ls.add(new MyContact("sdvfd","dvd","dsvfv"));
-        ls.add(new MyContact("sdvfd","dvd","dsvfv"));
-        ls.add(new MyContact("sdvfd","dvd","dsvfv"));
+        RequestQueue queue= Volley.newRequestQueue(MainActivity.this);
+        StringRequest request=new StringRequest(Request.Method.GET, "http://172.16.39.241/smdb/get.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject obj=new JSONObject(response);
+                            if(obj.getInt("code")==1)
+                            {
+                                JSONArray contacts=obj.getJSONArray("contacts");
+                                for (int i=0; i<contacts.length();i++)
+                                {
+                                    JSONObject contact=contacts.getJSONObject(i);
+                                    ls.add(new MyContact(contact.getString("name"),contact.getString("phno"),contact.getString("address")));
+                                    adapter.notifyDataSetChanged();
+                                }
 
+                            }
+                            else{
+                                Toast.makeText(MainActivity.this,obj.get("msg").toString(),Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,"Error Connecting Server",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        queue.add(request);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
