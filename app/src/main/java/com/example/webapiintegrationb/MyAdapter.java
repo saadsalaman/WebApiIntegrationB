5fea1e1d812e415a0245f164529e531dd08c2b76
Syed Muhammad Saad Salman <im.saadsalmanahashmi@gmail.com>
2022-11-10 10:32:32 +0500

    POST Request Included 2nd Commit

--- app/src/main/java/com/example/webapiintegrationb/MyAdapter.java
+++ app/src/main/java/com/example/webapiintegrationb/MyAdapter.java
@@ -1,10 +1,13 @@
 package com.example.webapiintegrationb;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -28,10 +31,18 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
         holder.name.setText(ls.get(position).getName());
         holder.phno.setText(ls.get(position).getPhno());
         holder.address.setText(ls.get(position).getAddress());
+        holder.roww.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(c,position+"",Toast.LENGTH_LONG).show();
+                ls.remove(position);
+                notifyDataSetChanged();
+            }
+        });
 
     }
 
@@ -42,11 +53,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView name,phno,address;
+        LinearLayout roww;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             name=itemView.findViewById(R.id.name);
             phno=itemView.findViewById(R.id.phno);
             address=itemView.findViewById(R.id.address);
+            roww=itemView.findViewById(R.id.roww);
         }
     }
 }
